8a5ebf34f736023a11552a3a8b982548bbb4e484
benk10 <ben.kaufman10@gmail.com>
2020-03-31 14:35:43 +0300

    Add eslint

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const ethers = require('ethers')
+const ethers = require('ethers');
 // Is the voteParams same for all/some schemes of a common?
 
 // TODO: Edit constants/ Make them function params
-const arcVersion = "0.1.1-rc.11"
+const arcVersion = "0.1.1-rc.11";
 
 function getForgeOrgData({
     DAOFactoryInstance,
@@ -13,7 +13,7 @@ function getForgeOrgData({
 }) {
     let daoTokenABI = require('./abis/DAOToken.json');
 
-    let daoToken = new ethers.utils.Interface(daoTokenABI)
+    let daoToken = new ethers.utils.Interface(daoTokenABI);
     const daoTokenArgs = Object.values({
         tokenName: '',
         tokenSymbol: '',
@@ -28,7 +28,7 @@ function getForgeOrgData({
         tokenDist,
         repDist,
         [0, 0, getArcVersionNumber(arcVersion)]
-    ]
+    ];
 }
 
 function getSetSchemesData({
@@ -49,9 +49,9 @@ function getSetSchemesData({
     let fundingRequestABI = require('./abis/FundingRequest.json');
     let schemeFactoryABI = require('./abis/SchemeFactory.json');
 
-    let joinAndQuit = new ethers.utils.Interface(joinAndQuitABI)
-    let fundingRequest = new ethers.utils.Interface(fundingRequestABI)
-    let schemeFactory = new ethers.utils.Interface(schemeFactoryABI)
+    let joinAndQuit = new ethers.utils.Interface(joinAndQuitABI);
+    let fundingRequest = new ethers.utils.Interface(fundingRequestABI);
+    let schemeFactory = new ethers.utils.Interface(schemeFactoryABI);
 
     const joinAndQuitArgs = Object.values({
         avatar,
@@ -93,24 +93,24 @@ function getSetSchemesData({
         ],
         ['0x00000000', '0x00000000', '0x0000001F'],
         metaData
-    ]
+    ];
 }
 
 // Helpers
 
 function concatBytes(bytes1, bytes2) {
-    return bytes1 + (bytes2.slice(2))
+    return bytes1 + (bytes2.slice(2));
 }
   
 function getBytesLength(bytes) {
-    return Number(bytes.slice(2).length) / 2
+    return Number(bytes.slice(2).length) / 2;
 }
 
 function getArcVersionNumber(arcVersion) {
-    return Number(arcVersion.split('rc.')[1])
+    return Number(arcVersion.split('rc.')[1]);
 }
 
 module.exports = {
     getForgeOrgData,
     getSetSchemesData
-}
+};
--- test/commonfactory.test.js
+++ test/commonfactory.test.js
@@ -1,12 +1,12 @@
 test('deploy common', async () => {
     // Import Script
-    const { getForgeOrgData, getSetSchemesData } = require('../index.js')
+    const { getForgeOrgData, getSetSchemesData } = require('../index.js');
     // Web3 setup
-    const Web3 = require('web3')
+    const Web3 = require('web3');
     const web3 = new Web3('http://localhost:8545', null, {
       transactionConfirmationBlocks: 1
-    })
-    web3.eth.accounts.wallet.clear()
+    });
+    web3.eth.accounts.wallet.clear();
     let privateKeys = [
         '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d',
         '0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1',
@@ -18,15 +18,15 @@ test('deploy common', async () => {
         '0xa453611d9419d0e56f499079478fd72c37b251a94bfde4d19872c44cf65386e3',
         '0x829e924fdf021ba3dbbc4225edfece9aca04b929d6e75613329ca6f1d31c0bb4',
         '0xb0057716d5917badaf911b193b12b910811c1497b5bada8d7711f758981c3773'
-    ]
+    ];
     for (let i = 0; i < privateKeys.length; i++) {
-        web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(privateKeys[i]))
+        web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(privateKeys[i]));
     }
     let opts = {
       from: web3.eth.accounts.wallet[0].address,
       gas: 6000000,
       gasPrice: 10
-    }
+    };
     // End Web3 setup
 
     // Test Common Setup 
@@ -51,10 +51,10 @@ test('deploy common', async () => {
       })
     ).send();
 
-    let newOrg = forgeOrg.events.NewOrg.returnValues
+    let newOrg = forgeOrg.events.NewOrg.returnValues;
 
-    let avatarAddress = newOrg._avatar
-    let reputationAddress = newOrg._reputation
+    let avatarAddress = newOrg._avatar;
+    let reputationAddress = newOrg._reputation;
 
     const avatar = new web3.eth.Contract(
       require('@daostack/migration-experimental/contracts/0.1.1-rc.11/Avatar.json').abi,
@@ -68,12 +68,12 @@ test('deploy common', async () => {
       opts
     );
 
-    expect(await avatar.methods.orgName().call()).toBe(orgName)
-    expect(await reputation.methods.balanceOf(web3.eth.accounts.wallet[0].address).call()).toBe('100')
-    expect(await reputation.methods.totalSupply().call()).toBe('100')
+    expect(await avatar.methods.orgName().call()).toBe(orgName);
+    expect(await reputation.methods.balanceOf(web3.eth.accounts.wallet[0].address).call()).toBe('100');
+    expect(await reputation.methods.totalSupply().call()).toBe('100');
     
-    const votingMachine = DAOstackMigration.migration('private').package['0.1.1-rc.11'].GenesisProtocol
-    const deadline = (await web3.eth.getBlock("latest")).timestamp + 3000
+    const votingMachine = DAOstackMigration.migration('private').package['0.1.1-rc.11'].GenesisProtocol;
+    const deadline = (await web3.eth.getBlock("latest")).timestamp + 3000;
     const setSchemes = await daoFactory.methods.setSchemes(
       ...getSetSchemesData({
         DAOFactoryInstance,
@@ -91,7 +91,7 @@ test('deploy common', async () => {
     })
   ).send();
 
-  let schemesEvents = setSchemes.events.SchemeInstance
+  let schemesEvents = setSchemes.events.SchemeInstance;
 
   const joinAndQuit = new web3.eth.Contract(
     require('../abis/JoinAndQuit.json'),
@@ -111,23 +111,23 @@ test('deploy common', async () => {
     opts
   );
 
-  expect(await joinAndQuit.methods.avatar().call()).toBe(avatarAddress)
-  expect(await joinAndQuit.methods.votingMachine().call()).toBe(votingMachine)
-  expect(await joinAndQuit.methods.voteParams().call()).toBe("0x1000000000000000000000000000000000000000000000000000000000000000")
-  expect(await joinAndQuit.methods.fundingToken().call()).toBe("0x0000000000000000000000000000000000000000")
-  expect(await joinAndQuit.methods.minFeeToJoin().call()).toBe("100")
-  expect(await joinAndQuit.methods.memberReputation().call()).toBe("100")
-  expect(await joinAndQuit.methods.fundingGoal().call()).toBe("1000")
-  expect(await joinAndQuit.methods.fundingGoalDeadLine().call()).toBe(deadline.toString())
+  expect(await joinAndQuit.methods.avatar().call()).toBe(avatarAddress);
+  expect(await joinAndQuit.methods.votingMachine().call()).toBe(votingMachine);
+  expect(await joinAndQuit.methods.voteParams().call()).toBe("0x1000000000000000000000000000000000000000000000000000000000000000");
+  expect(await joinAndQuit.methods.fundingToken().call()).toBe("0x0000000000000000000000000000000000000000");
+  expect(await joinAndQuit.methods.minFeeToJoin().call()).toBe("100");
+  expect(await joinAndQuit.methods.memberReputation().call()).toBe("100");
+  expect(await joinAndQuit.methods.fundingGoal().call()).toBe("1000");
+  expect(await joinAndQuit.methods.fundingGoalDeadLine().call()).toBe(deadline.toString());
   
-  expect(await fundingRequest.methods.avatar().call()).toBe(avatarAddress)
-  expect(await fundingRequest.methods.votingMachine().call()).toBe(votingMachine)
-  expect(await fundingRequest.methods.voteParams().call()).toBe("0x1100000000000000000000000000000000000000000000000000000000000000")
-  expect(await fundingRequest.methods.fundingToken().call()).toBe("0x0000000000000000000000000000000000000000") 
+  expect(await fundingRequest.methods.avatar().call()).toBe(avatarAddress);
+  expect(await fundingRequest.methods.votingMachine().call()).toBe(votingMachine);
+  expect(await fundingRequest.methods.voteParams().call()).toBe("0x1100000000000000000000000000000000000000000000000000000000000000");
+  expect(await fundingRequest.methods.fundingToken().call()).toBe("0x0000000000000000000000000000000000000000"); 
 
 
-  expect(await schemeFactory.methods.avatar().call()).toBe(avatarAddress)
-  expect(await schemeFactory.methods.votingMachine().call()).toBe(votingMachine)
-  expect(await schemeFactory.methods.voteParams().call()).toBe("0x1110000000000000000000000000000000000000000000000000000000000000")
-  expect(await schemeFactory.methods.daoFactory().call()).toBe(DAOFactoryInstance) 
+  expect(await schemeFactory.methods.avatar().call()).toBe(avatarAddress);
+  expect(await schemeFactory.methods.votingMachine().call()).toBe(votingMachine);
+  expect(await schemeFactory.methods.voteParams().call()).toBe("0x1110000000000000000000000000000000000000000000000000000000000000");
+  expect(await schemeFactory.methods.daoFactory().call()).toBe(DAOFactoryInstance); 
 }, 500000);
